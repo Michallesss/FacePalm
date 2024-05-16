b607f02124a03ab413eaf3397c6162c6770e3ca9
Mikeal <mwiemszra@gmail.com>
2024-05-16 21:25:08 +0200

    💡 Add comment with ref

--- src/app/sign/page.tsx
+++ src/app/sign/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 // import SignUp from './up/page';
 // import SignIn from './in/page';
 
-export default function Sign() {
+export default function Sign() { // https://tailwindui.com/components/application-ui/forms/sign-in-forms
   // return <SignIn />;
   return redirect('/sign/in');
 }

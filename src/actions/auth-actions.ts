94cc70b0a61495a2cee3a0e09f81576a67230778
Michallesss <mwiemszra@gmail.com>
2024-05-22 16:05:18 +0200

    🚑 Set api url to public for actions

--- src/actions/auth-actions.ts
+++ src/actions/auth-actions.ts
@@ -11,7 +11,7 @@ import {
 const config = {
   maxAge: 60 * 60 * 24 * 7, // 1 week
   path: "/",
-  domain: process.env.API_URL ?? "localhost",
+  domain: process.env.NEXT_PUBLIC_API_URL ?? "localhost",
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
 };

9d0be3fcbce41c2239b6451d1fd41272dbd8ffc0
Michallesss <mwiemszra@gmail.com>
2024-05-25 12:46:57 +0200

    ⚰️ Remove useless import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getUserMeLoader } from "@/services/get-user-me-loader";
-import { use } from "react";
 
 export async function middleware(request: NextRequest) {
   const user = await getUserMeLoader();

71b6945685dd087afec9a787a9684a698280dc11
Michallesss <mwiemszra@gmail.com>
2024-05-26 22:57:17 +0200

    ✨ profile page

--- src/services/profile-services.ts
+++ src/services/profile-services.ts
@@ -1,5 +1,23 @@
 'use server';
 
+const baseUrl = process.env.API_URL;
+
 export async function getProfileService(profileId: string) {
-  
+  const url = new URL(`/api/users/${profileId}`, baseUrl);
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // body: JSON.stringify({}),
+      cache: "no-cache",
+    });
+
+    return response.json();
+  } catch (error) {
+    console.error("Get Posts Service Error:", error);
+    // throw error;
+  }
 }

e45b0219c919d071b3dc3d57753b5f1608a17df2
Michallesss <mwiemszra@gmail.com>
2024-05-27 11:07:30 +0200

    ✏️ Wrong .env variable import typo bug fix

--- src/services/comment-services.ts
+++ src/services/comment-services.ts
@@ -1,21 +1,25 @@
 'use server';
 
+import { getAuthToken } from "@/services/get-token";
+
 export interface CreateCommentProps {
   author: number;
   content: string;
   post: string;
 };
 
-const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+const baseUrl = process.env.API_URL;
 
 export async function createCommentService(commentData: CreateCommentProps) {
   const url = new URL("/api/comments", baseUrl);
+  const authToken = getAuthToken();
 
   try {
     const response = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
       },
       body: JSON.stringify({ ...commentData }),
       cache: "no-cache",
